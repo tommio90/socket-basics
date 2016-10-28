1d17e4e3ad765f926787ecd8672169be593151e5
Giuseppe Tomasello <gtomasello90@gmail.com>
2016-10-28 15:52:26 +0800

    sending live data back and forth

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,9 @@ var socket= io();
 socket.on('connected', function(){
     console.log('Connected with socket.io');
 });
+
+socket.on('message', function(message){
+    console.log('New message:');
+    console.log(message.text);
+
+})
--- server.js
+++ server.js
@@ -6,8 +6,18 @@ var http = require('http').Server(app);
 var io = require ('socket.io')(http)
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(){
-console.log('Hello socket.io!');
+io.on('connection', function(socket){
+    console.log('Hello socket.io!');
+
+    socket.on('message', function(message){
+        console.log('Message received: '+ message.text);
+       
+        socket.broadcast.emit('message', message);
+    });
+
+    socket.emit('message', {
+        text: 'Welcome to the chat super application'
+    });
 });
 
 http.listen(PORT, function(){
