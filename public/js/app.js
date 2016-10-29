21c84c588a3473e3e4e804d056a0cdb2c6de3356
Giuseppe Tomasello <gtomasello90@gmail.com>
2016-10-29 19:25:14 +0800

    add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,23 @@
-var socket= io();
-
+    var socket= io();
+    var name = getQueryVariable('name') || 'Anonymous';
+    var room = getQueryVariable('room');
 socket.on('connected', function(){
     console.log('Connected with socket.io');
 });
+
+
+ jQuery('#location').append('<p> <strong>' +name+' : </strong>  Joined the room:  ' +room+ '</p>');
 
 socket.on('message', function(message){
     
     var momentTimestamp = moment.utc(message.timestamp);
     var date = momentTimestamp.local().format(' h:mm:ss a');
+    var $messages =jQuery('.messages');
+    
     console.log('New message:');
     console.log(message.text);
 
-    jQuery('.messages').append('<p> <strong>' +date+' : </strong>    ' +message.text+ '</p>');
+    $messages.append('<p> <strong>' +message.name+' :    '+date+ '</strong>    ' +message.text+ '</p>');
 
 });
 
@@ -24,6 +30,7 @@ $form.on('submit', function(event){
 
     var $message = $form.find('input[name=message]');
     socket.emit('message', {
+        name: name,
         text: $message.val()
     });
 
